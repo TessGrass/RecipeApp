d731fe9e131114ce3b9651e310a37cf892d6be84
Therese Grass <tg222kv@student.lnu.se>
2022-12-16 16:29:25 +0100

    add styling to gridTile and categorysScreen

--- components/categoryGridTile.js
+++ components/categoryGridTile.js
@@ -1,8 +1,8 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function categoryGridTile({ title, color }) {
   return (
-    <View>
+    <View style={styles.gridContainer} backgroundColor={color}>
       <Pressable>
         <View>
           <Text>{title}</Text>
@@ -13,3 +13,13 @@ function categoryGridTile({ title, color }) {
 }
 
 export default categoryGridTile
+
+const styles = StyleSheet.create({ // change to Dimensions API
+  gridContainer: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+  },
+})
--- screens/categoriesScreen.js
+++ screens/categoriesScreen.js
@@ -1,15 +1,21 @@
 import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/categoryGridTile'
 import { CATEGORIES } from '../data/dummy-data';
 
-function renderCategoryItem(item) { // declared outside component because it doesn't need to be re-created when the component is re-rendered.
+function renderCategoryItem(itemData) { // declared outside component because it doesn't need to be re-created when the component is re-rendered.
   return (
-    
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
   )
 }
 
+//Flatlist makes it scrollable, flatlist isn't neccessary here due to the fact that the list is a fixed size, scrollable could work as well.
 function categoriesScreen() {
   return (
-    <FlatList data={CATEGORIES} keyExtractor={(category) => category.id} /> 
+    <FlatList data={CATEGORIES}
+    keyExtractor={(category) => category.id} 
+    renderItem={renderCategoryItem} 
+    numColumns={2}
+    /> 
   )
 
 }
