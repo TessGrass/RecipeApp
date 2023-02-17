4db43e0a5fdd738c3e0745ec25a2a2712e75d082
Therese Grass <tg222kv@student.lnu.se>
2023-02-17 13:51:37 +0100

    add mealItem component and image prop

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -4,14 +4,14 @@ import MealItem from '../components/MealItem'
 
 function MealsOverViewScreen({ route }) { // route comes from mealsoverview being registered as a screen in app.js
   const categoryId = route.params.categoryId // params - an optional object containing params which is defined while navigating. In this case it's itemData.item.id.
-  
+
   const displayMeals = MEALS.filter((mealItem) => {
-    return mealItem.categoryIds.indexOf(categoryId)
+    return mealItem.categoryIds.indexOf(categoryId) >= 0 // returns true if categoryId matches MEALS.categoryIds, if the index doesnt match it will return -1.
   })
 
   function renderMealItem(itemData) {
     return (
-     <MealItem title={itemData.item.title} />
+     <MealItem title={itemData.item.title} imageUrl={itemData.item.imageUrl} />
     )
   }
 
