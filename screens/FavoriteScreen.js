39ea6560bea909345d372c5f285a4183d0bf4c34
Therese Grass <tg222kv@student.lnu.se>
2023-02-23 21:06:15 +0100

    display message if favorite list is empty

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,15 +1,38 @@
 import { useContext } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
 import { FavoritesContext } from '../store/context/favorites-context.js'
 import { MEALS } from '../data/dummy-data'
 import MealsList from '../components/MealsList/MealsList'
+
 
 function FavoriteScreen() {
   
   const favoriteMealCtx = useContext(FavoritesContext)
   const favoriteMeals = MEALS.filter(meal => favoriteMealCtx.ids.includes(meal.id)) // returns an array with all meals.id that matches id's found in the favoritMealsCtx
   
+  if (favoriteMeals.length === 0) {
+    return <View style={styles.rootContainer}>
+      <Text style={styles.infoText}>You have no favorite meals yet</Text>
+    </View>
+  } else {
   return <MealsList items={favoriteMeals} />
-  
+  }  
 }
 
 export default FavoriteScreen
+
+const styles = StyleSheet.create({
+
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center', // Vertically
+    alignItems: 'center'     // Horizontally
+  },
+
+  infoText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  }
+
+})
